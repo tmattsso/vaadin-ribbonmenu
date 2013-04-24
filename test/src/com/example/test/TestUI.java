58f4faaa970bb26d4626e11bd7c52be2834d8afc
Thomas <thomas@vaadin.com>
2013-04-24 15:59:11 +0300

    makes onerow buttons 100px wide

--- test/src/com/example/test/TestUI.java
+++ test/src/com/example/test/TestUI.java
@@ -7,7 +7,6 @@ import com.merap.promoren.component.officemenu.MenuSection;
 import com.merap.promoren.component.officemenu.MenuTab;
 import com.merap.promoren.component.officemenu.OfficeMenu;
 import com.merap.promoren.component.officemenu.SubMenuItem;
-import com.merap.promoren.component.officemenu.ToggleMenuItem;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
@@ -31,6 +30,14 @@ public class TestUI extends UI {
 
 		OfficeMenu menu = new OfficeMenu();
 
+		MenuCommand dummyCommand = new MenuCommand() {
+
+			@Override
+			public void menuItemClicked(SubMenuItem item) {
+				Notification.show("HELLO!");
+			}
+		};
+
 		menu.addEmptySection("File", new ThemeResource("images/add.png"),
 				new MenuCommand() {
 
@@ -40,40 +47,50 @@ public class TestUI extends UI {
 					}
 				});
 
-		MenuTab tab = menu.addTab("Create");
+		/* TASK */
+		MenuTab tab = menu.addTab("Task");
 
-		MenuSection section = tab.addSection("Create");
-		section.addItem(new ToggleMenuItem("Toggle me", new ThemeResource(
-				"images/add.png"), null));
-		section.addItem("Create longer", new ThemeResource("images/add.png"));
-		section.addItem("Create a really long thing", new ThemeResource(
-				"images/add.png"));
-
-		section = tab.addSection("Save");
-		section.addItem("Save", new ThemeResource("images/add.png"));
-		section.addItem("Save As", new ThemeResource("images/add.png"));
-		section.addItem("Save Copy As", new ThemeResource("images/add.png"));
+		MenuSection section = tab.addSection("New");
+		MenuItem item = section.addItem("New from Favourites",
+				new ThemeResource("images/add.png"));
+		item.addSubItem("Favourite 1", new ThemeResource("images/add.png"),
+				dummyCommand);
 
 		GridMenuSection grid = tab.addGridSection("Wizard", 1, 2);
 		grid.addItem("View", new ThemeResource("images/add.png"), 0, 0);
 		grid.addItem("Edit", new ThemeResource("images/add.png"), 0, 1);
 
-		grid = tab.addGridSection("Test", 2, 2);
-		grid.addItem("View", new ThemeResource("images/add.png"), 0, 0);
-		MenuItem item = grid.addItem("Edit",
-				new ThemeResource("images/add.png"), 0, 1);
+		section = tab.addSection("Notes");
+		section.addItem("View / edit", new ThemeResource("images/add.png"));
 
-		item.addSubItem("subItem2", new ThemeResource("images/add.png"),
-				new MenuCommand() {
+		section = tab.addSection("Barcodes");
+		section.addItem("Manage", new ThemeResource("images/add.png"));
 
-					@Override
-					public void menuItemClicked(SubMenuItem item) {
-						Notification.show("HELLO!");
-					}
-				});
+		grid = tab.addGridSection("Calendar", 2, 2);
+		item = grid.addItem("Go To...", new ThemeResource("images/add.png"), 0,
+				0);
+		item.addSubItem("Go to 1", new ThemeResource("images/add.png"),
+				dummyCommand);
+		grid.addItem("View Month", new ThemeResource("images/add.png"), 0, 1);
+		grid.addItem("New Event", new ThemeResource("images/add.png"), 1, 0, 1,
+				1);
 
-		grid.addItem("Edit", new ThemeResource("images/add.png"), 1, 0, 1, 1);
+		grid = tab.addGridSection("Manage tasks", 1, 2);
+		grid.addItem("Unfollow", new ThemeResource("images/add.png"), 0, 0);
+		grid.addItem("Archive", new ThemeResource("images/add.png"), 0, 1);
 
+		grid = tab.addGridSection("Workflow", 3, 2);
+		grid.addItem("Local History", new ThemeResource("images/add.png"), 0, 0);
+		grid.addItem("Global History", new ThemeResource("images/add.png"), 0,
+				1);
+		item = grid.addItem("Forward to..",
+				new ThemeResource("images/add.png"), 1, 0, 1, 1);
+		item.addSubItem("User 1", new ThemeResource("images/add.png"),
+				dummyCommand);
+		grid.addItem("Auto Forward", new ThemeResource("images/add.png"), 2, 0,
+				2, 1);
+
+		/* DOCS */
 		tab = menu.addTab("Edit");
 
 		section = tab.addSection("Clipboard");
